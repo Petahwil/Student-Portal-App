2e0a5dd5d77697f7d00cb708567684f0e0803914
Brianne Bindas <briannebindas@gmail.com>
2017-03-01 01:01:41 -0500

    added signature box

--- TimeSheet/app/src/main/java/com/example/dpivonka/timesheet/MainActivity.java
+++ TimeSheet/app/src/main/java/com/example/dpivonka/timesheet/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.dpivonka.timesheet;
 
+import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.gcacace.signaturepad.views.SignaturePad;
 
 import it.enricocandino.androidmail.MailSender;
 import it.enricocandino.androidmail.model.Mail;
@@ -24,6 +31,36 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         mailSender.sendMail(mail);
+
+        SignaturePad mSignaturePad;
+        mSignaturePad = (SignaturePad) findViewById(R.id.signature_pad);
+        mSignaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+
+            @Override
+            public void onStartSigning() {
+                //Event triggered when the pad is touched
+            }
+
+            @Override
+            public void onSigned() {
+                //Event triggered when the pad is signed
+            }
+
+            @Override
+            public void onClear() {
+                //Event triggered when the pad is cleared
+            }
+        });
+
+        final Button button = (Button) findViewById(R.id.button);
+
+        //if button is clicked
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Signature has been submitted.", Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
