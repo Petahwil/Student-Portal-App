f579edd6c7dd9a8e6791f2a34531d330d9b16db9
Wilson <Peter.Wilson2@emc.com>
2017-03-06 16:53:15 -0500

    more firebase

--- TimeSheetWebClient/app/src/main/java/com/example/samjc/timesheetwebclient/MainActivity.java
+++ TimeSheetWebClient/app/src/main/java/com/example/samjc/timesheetwebclient/MainActivity.java
@@ -11,23 +11,35 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    //firebase
+    private FirebaseDatabase mFirebaseDatabase;
+    //firebase
+    private DatabaseReference mEmployeeDatabaseReference;
+    //firebase
+    private void writeNewUser(String name, String email) {
+        User user = new User(name, email);
+
+        mEmployeeDatabaseReference.push().setValue(user);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //firebase
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        //firebase
+        mEmployeeDatabaseReference = mFirebaseDatabase.getReference().child("employees");
 
         final ListView lvMain = (ListView) findViewById(R.id.lv_main);
-        myRef.setValue("Hello, World!");
         ArrayList<Person> fakePeople = new ArrayList<Person>();
         fakePeople.add(new Person("Person1", "email1@domain.com"));
         fakePeople.add(new Person("Person2", "email2@domain.com"));
         fakePeople.add(new Person("Person3", "email3@domain.com"));
         PersonAdapter adapter = new PersonAdapter(this, fakePeople);
         lvMain.setAdapter(adapter);
-
+        //firebase
+        writeNewUser("user name" , "email");
         //startActivity(new Intent(MainActivity.this, AddNewPersonActivity.class));
     }
 
